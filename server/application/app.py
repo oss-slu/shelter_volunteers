c9ab75090684f9587b9385aae51701fa77652e13
Kate Holdener <kate.holdener@slu.edu>
2023-09-28 10:30:42 -0500

    flask side updates

--- server/application/app.py
+++ server/application/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from application.rest import work_shift
+
 #from flask_cors import CORS, cross_origin
 
 #CLIENT_URL = "http://localhost:3000" # URL of the React app
@@ -8,7 +10,7 @@ def create_app(config_name):
    app = Flask(__name__)
    config_module = f"application.config.{config_name.capitalize()}Config"
    app.config.from_object(config_module)
-   app.register_blueprint(room.blueprint)
+   app.register_blueprint(work_shift.blueprint)
    return app
 
 
--- server/run_dev_server.sh
+++ server/run_dev_server.sh
@@ -1,4 +1,4 @@
 export FLASK_APP=app
 export FLASK_ENV=development
-export FLASK_CONFIG="
+export FLASK_CONFIG="development"
 flask run
