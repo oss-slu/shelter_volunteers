aa43ce63597634d5dba15c17bcddf68d7b089b72
kate-holdener <42073871+kate-holdener@users.noreply.github.com>
2023-09-28 14:51:29 -0500

    Update server/wsgi.py to include multiple CLIENT URLS for CORS
    Co-authored-by: Abhilash Kotha <113061137+AbhilashKotha@users.noreply.github.com>

--- server/wsgi.py
+++ server/wsgi.py
@@ -5,5 +5,5 @@ from application.app import create_app
 
 app = create_app(os.environ["FLASK_CONFIG"])
 
-CLIENT_URL = "http://localhost:3000" # URL of the React app
+CLIENT_URLS = ["http://localhost:3000","http://127.0.0.1:3000"] URL of the React app
 cors = CORS(app, resources={r"*": {"origins": CLIENT_URL}})
