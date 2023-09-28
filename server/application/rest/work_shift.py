4761c55ab57d8524381af484b346d776589f3f4f
Kate Holdener <kate.holdener@slu.edu>
2023-09-28 14:54:52 -0500

    added real time stamps to the canned data

--- server/application/rest/work_shift.py
+++ server/application/rest/work_shift.py
@@ -14,15 +14,15 @@ shifts = [
       "code":"f853578c-fc0f-4e65-81b8-566c5dffa35a",
       "worker":"volunteer@slu.edu",
       "shelter":"shelter-id-for-st-patric-center",
-      "start_time": 1695913039455,
-      "end_time": 1695913049455
+      "start_time": 1696168800000,
+      "end_time": 1696179600000
    },
    {
       "code":"f853578c-fc0f-4e65-81b8-566c5dffa35b",
       "worker":"volunteer2@slu.edu",
       "shelter":"shelter-id-for-st-patric-center",
-      "start_time": 1695914039455,
-      "end_time": 1695914049455
+      "start_time": 1696255200000,
+      "end_time": 1696269600000
    }
 ]
 
