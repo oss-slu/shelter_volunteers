eab8ba1dd979429aaca9a4683671b8a437d10904
Kate Holdener <kate.holdener@slu.edu>
2023-09-28 09:52:43 -0500

    added memrepo

---
+++ server/repository/memrepo.py
@@ -0,0 +1,9 @@
+from domain.work_shift import WorkShift
+
+
+class MemRepo:
+   def __init__(self, data):
+      self.data = data
+
+   def list(self):
+      return [WorkShift.from_dict(i) for i in self.data]
