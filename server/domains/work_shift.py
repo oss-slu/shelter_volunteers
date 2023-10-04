6aa7b5d886d9ac1088c7030680d185ad4a0206a8
Abhilash Kotha <abhilash.kotha@slu.edu>
2023-10-04 14:40:45 -0500

    addressed linting comments

--- server/domains/work_shift.py
+++ server/domains/work_shift.py
@@ -19,12 +19,14 @@ class WorkShift:
     @classmethod
     def from_dict(self, d):
         """
-        The function is a class method that takes in a dictionary and returns an instance of the class.
+        The function is a class method that takes in a dictionary
+        and returns an instance of the class.
         """
         return self(**d)
 
     def to_dict(self):
         """
-        The function takes an object and returns a dictionary representation of the object.
+        The function takes an object and returns a dictionary
+        representation of the object.
         """
         return dataclasses.asdict(self)
--- server/use_cases/list_workshifts.py
+++ server/use_cases/list_workshifts.py
@@ -1,3 +1,6 @@
+"""
+This module contains the use case for listing work shifts.
+"""
 def workshift_list_use_case(repo):
     """
     The function returns a list of all workshifts in the in-memory database.
