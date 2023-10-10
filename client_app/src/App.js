be683082eff173a6d0ea84aac3b7c4577ae21375
loganwyas <loganw9999@gmail.com>
2023-10-10 13:36:55 -0500

    WIP (Add minimal navigation)

--- client_app/src/App.js
+++ client_app/src/App.js
@@ -1,9 +1,31 @@
-import Shifts from "./Shifts";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+} from "react-router-dom";
+
 import Shelters from "./Shelters";
+import Shifts from "./Shifts";
+
 import "./App.css";
 
 function App() {
-  return Shelters();
+  return (
+    <div>
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/shelters">Shelters</Link>
+        </div>
+        <Routes>
+          <Route exact path="/" element={<Shifts />} />
+          <Route path="/shelters" element={<Shelters />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
