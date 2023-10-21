80eaae1e4624a85012150be0c7accabb351f9d31
sai9797 <saiteja.gollapalli@slu.edu>
2023-10-21 11:02:41 -0500

    resolve pylint errors

--- server/tests/rest/test_work_shift.py
+++ server/tests/rest/test_work_shift.py
@@ -1,10 +1,32 @@
 """Tests for work shift related REST endpoints."""
 
 import json
+from unittest import mock
 from application.app import create_app
+from responses import ResponseSuccess
 
+shifts_data = [
+    {
+        'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35a',
+        'worker': 'volunteer@slu.edu',
+        'shelter': 'existing-shelter-id',
+        'start_time': 1701441800000,
+        'end_time': 1701452600000
+    },
+    {
+        'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35b',
+        'worker': 'volunteer@slu.edu',
+        'shelter': 'existing-shelter-id',
+        'start_time': 1701442800000,
+        'end_time': 1701453600000
+    }
+]
 
-def test_list_work_shifts():
+@mock.patch('application.app.work_shift.workshift_list_use_case')
+def test_list_work_shifts(mock_use_case):
+    mock_response = ResponseSuccess(shifts_data)
+    mock_use_case.return_value = mock_response
+
     app = create_app('testing')
     client = app.test_client()
     headers = {
@@ -13,11 +35,23 @@ def test_list_work_shifts():
     response = client.get('/shifts', headers=headers)
     data = json.loads(response.data)
 
+    assert data == shifts_data
     assert response.status_code == 200
-    assert len(data) == 2
+    mock_use_case.assert_called()
 
 
-def test_add_work_shifts():
+@mock.patch('application.app.work_shift.workshift_add_multiple_use_case')
+def test_add_work_shifts(mock_use_case):
+    mock_use_case.return_value = [
+        {
+            'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35d',
+            'worker': 'volunteer@slu.edu',
+            'shelter': 'new-shelter-id',
+            'start_time': 1701442800000,
+            'end_time': 1701453600000
+        }
+    ]
+
     app = create_app('testing')
     client = app.test_client()
     new_shift = {
@@ -38,9 +72,14 @@ def test_add_work_shifts():
 
     assert response.status_code == 200
     assert data[0]['code'] == 'f853578c-fc0f-4e65-81b8-566c5dffa35d'
+    mock_use_case.assert_called()
 
 
-def test_delete_work_shift():
+@mock.patch('application.app.work_shift.delete_shift_use_case')
+def test_delete_work_shift(mock_use_case):
+    mock_response = ResponseSuccess({'message': 'Shift deleted successfully'})
+    mock_use_case.return_value = mock_response
+
     app = create_app('testing')
     client = app.test_client()
     headers = {'Authorization': 'volunteer@slu.edu'}
@@ -48,3 +87,6 @@ def test_delete_work_shift():
         '/shifts/f853578c-fc0f-4e65-81b8-566c5dffa35a', headers=headers
     )
     assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data['message'] == 'Shift deleted successfully'
+    mock_use_case.assert_called()
