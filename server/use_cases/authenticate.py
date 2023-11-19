0dacff2fd6222fb6a25df37d5d519faf58532088
Kate Holdener <kate.holdener@slu.edu>
2023-11-19 15:32:02 -0600

    added starter code for authenticate use case

---
+++ server/use_cases/authenticate.py
@@ -0,0 +1,8 @@
+def login(username, password):
+    """
+    The login function contacts GetHelp authentication API to retrieve
+    a token for the given username and password. We are doing this on the server
+    because login through GetHelp requires the use of GetHelp API token. We don't
+    want to store this token on the client side.
+    """
+    pass
