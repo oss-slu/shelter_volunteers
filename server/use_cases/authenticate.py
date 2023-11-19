7431f555cd78dc4d2caa81b45808d1f486bf4ebb
Kate Holdener <kate.holdener@slu.edu>
2023-11-19 15:42:36 -0600

    added a login usecase

--- server/use_cases/authenticate.py
+++ server/use_cases/authenticate.py
@@ -1,8 +1,44 @@
-def login(username, password):
+"""
+This module contains authentication related functions.
+"""
+import requests
+import base64
+import os
+
+from responses import ResponseSuccess, ResponseFailure
+
+def login(user, password):
     """
     The login function contacts GetHelp authentication API to retrieve
-    a token for the given username and password. We are doing this on the server
-    because login through GetHelp requires the use of GetHelp API token. We don't
+    a token for the given username and password. 
+    We are doing this on the server because login through GetHelp 
+    requires the use of GetHelp API token. We don't
     want to store this token on the client side.
     """
-    pass
+
+    url = 'https://oauth-qa.gethelp.com/api/oauth/token'
+    getHelpToken = os.environ['REACT_APP_GETHELP_AUTH_API_TOKEN']
+
+    # Encode the authorization token
+    auth_token = base64.b64encode(getHelpToken.encode()).decode('utf-8')
+
+    # Set up the headers and data
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': f'Basic {auth_token}'
+    }
+    data = {
+        'grant_type': 'password',
+        'username': user,
+        'password': password
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+
+    if not response.ok:
+        return ResponseFailure(response.status_code,
+            "Unable to log in")
+
+    # Parse the JSON response
+    return ResponseSuccess(response.json())
+
