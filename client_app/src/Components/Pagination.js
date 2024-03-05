74cb317f1f17d87b56adfc661f594b65052a66a1
vakhil-98 <akhil.vemulapally@slu.edu>
2024-03-04 20:39:40 -0600

    Add pagination component

---
+++ client_app/src/Components/Pagination.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Pagination.css";
+
+export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const handlePageChange = (pageNumber) => {
+    onPageChange(pageNumber);
+  };
+
+  return (
+    <div className="pagination-wrapper">
+      <div className="pagination">
+        <button
+          className="pagination-button"
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span className="pagination-current-page">{currentPage}</span>
+        <span className="pagination-total-pages">of {totalPages}</span>
+        <button
+          className="pagination-button"
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
