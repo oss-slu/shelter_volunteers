d9131effb2f36a7022f838d0e760e76afc9ac358
vakhil-98 <akhil.vemulapally@slu.edu>
2024-04-19 23:05:38 -0500

    NavBar for shelter and volunteer view fixed as per issue description.

--- client_app/src/App.js
+++ client_app/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PastShifts, UpcomingShifts } from "./Shifts";
 import Shelters from "./Shelters";
 import VolunteerDashboard from "./VolunteerDashboard";
-import NavBar from "./Components/NavBar";
+import NavBarVolunteerDashboard from "./Components/NavBarVolunteerDashboard";
+import NavBarShelterDashboard from "./Components/NavBarShelterDashboard"
 import Login from "./Components/authentication/Login";
 import Logout from "./Components/authentication/Logout";
 import SignUp from "./Components/authentication/SignUp";
 import ProtectedRoute from "./ProtectedRoute";
 import React,{useState} from "react";
-import ShelterDashboard from "./ShelterDashboard"; 
+import ShelterDashboard from "./ShelterDashboard"
 
 import "./App.css";
 
@@ -17,7 +18,11 @@ function App() {
   return (
     <>      
       <Router>
-        <NavBar auth={auth}/>
+        {['/shelter-dashboard', '/shift-details', '/request-for-help', '/cancel-shifts'].includes(window.location.pathname) ? (
+            <NavBarShelterDashboard auth={auth} />
+        ) : (
+            <NavBarVolunteerDashboard auth={auth} />
+        )}
         <Routes>
           <Route path="/" element={<Login setAuth={setAuth}/>} />
           <Route path="/signup" element={<SignUp />} />
@@ -27,8 +32,8 @@ function App() {
               <Route path="/shelters" element={<Shelters />} />
               <Route path="/past-shifts" element={<PastShifts />} />
               <Route path="/upcoming-shifts" element={<UpcomingShifts />} />
-              <Route path="/shelterdashboard" element={<ShelterDashboard />} />
               <Route path="/logout" element={<Logout />} />
+              <Route path="/shelter-dashboard" element={<ShelterDashboard/>}/>
           </Route>
         </Routes>
       </Router>
