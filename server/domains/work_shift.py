bbdd4937cf669eef6328822c09efd5e665b6c8b7
strejoarciles <strejoarciles@slu.edu>
2025-01-29 21:19:15 -0600

    update struct rename

--- server/domains/work_shift.py
+++ server/domains/work_shift.py
@@ -1,14 +1,13 @@
-"""
-This module handles data converstion from dictionary to class obj or vice versa
-"""
 import uuid
 import dataclasses
+import warnings
+from deprecated import deprecated
 
-
+@deprecated(reason="WorkShift is deprecated and will be replaced by ServiceCommitment.")
 @dataclasses.dataclass
 class WorkShift:
     """
-    Data class for workshift releated data
+    [DEPRECATED] Data class for work shift-related data.
     """
     worker: str
     first_name: str
@@ -21,21 +20,59 @@ class WorkShift:
     def get_id(self):
         """Returns the ID of the work shift."""
         return self._id
+
     def set_id(self, new_id):
         """Sets the ID of the work shift."""
         self._id = new_id
 
     @classmethod
-    def from_dict(self, d):
-        """
-        The function is a class method that takes in a dictionary
-        and returns an instance of the class.
-        """
-        return self(**d)
+    def from_dict(cls, d):
+        warnings.warn("WorkShift is deprecated. Use ServiceCommitment instead.", DeprecationWarning)
+        return cls(**d)
 
     def to_dict(self):
-        """
-        The function takes an object and returns a dictionary
-        representation of the object.
-        """
+        warnings.warn("WorkShift is deprecated. Use ServiceCommitment instead.", DeprecationWarning)
+        return dataclasses.asdict(self)
+
+# New ServiceShift class replacing Work
+@dataclasses.dataclass
+class ServiceShift:
+    """
+    Data class for shelter-defined service shifts.
+    """
+    shelter: int
+    start_time: int  # number of milliseconds since the Epoch in UTC
+    end_time: int
+    volunteers_needed: int
+    _id: uuid.UUID = None
+
+    def get_id(self):
+        """Returns the ID of the service shift."""
+        return self._id
+
+    def set_id(self, new_id):
+        """Sets the ID of the service shift."""
+        self._id = new_id
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(**d)
+
+    def to_dict(self):
+        return dataclasses.asdict(self)
+
+# New ServiceCommitment class for volunteer sign-ups
+@dataclasses.dataclass
+class ServiceCommitment:
+    """
+    Data class for tracking volunteer commitments to service shifts.
+    """
+    volunteer_id: str  # Email or unique identifier
+    service_shift_id: uuid.UUID
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(**d)
+
+    def to_dict(self):
         return dataclasses.asdict(self)
