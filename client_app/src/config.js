b6f0ab4fc5f394e8ea366de5f49cb5cf35f4569e
Kate Holdener <kate.holdener@slu.edu>
2025-02-07 08:39:47 -0600

    finally properly configered REACT_APP_SERVER_URL for dev and production

--- client_app/src/config.js
+++ client_app/src/config.js
@@ -1,5 +1,3 @@
-export const SERVER = ""
-//   ? process.env.REACT_APP_SERVER_URL 
-//    : "http://127.0.0.1:5000";
+export const SERVER = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000/api'
 export const GETHELP_API = "https://api2-qa.gethelp.com/";
 export const ENVIROMENT = "development";
