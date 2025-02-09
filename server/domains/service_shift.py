61097137ffa66e40ca8e0b44ee1493c433fdd79c
strejoarciles <strejoarciles@slu.edu>
2025-02-08 23:31:15 -0600

    fixed linting

--- server/domains/service_shift.py
+++ server/domains/service_shift.py
@@ -1,14 +1,13 @@
 """
-This module handles data converstion from dictionary to class obj or vice versa
+This module handles data conversion from dictionary to class obj or vice versa.
 """
 import uuid
 import dataclasses
 
-
 @dataclasses.dataclass
-class ServiceShift: #renamed ServiceShift from Work 
+class ServiceShift:  # renamed ServiceShift from Work
     """
-    Data class for workshift releated data
+    Data class for work shift-related data.
     """
     shelter_id: int
     shift_name: str
@@ -22,22 +21,20 @@ class ServiceShift: #renamed ServiceShift from Work
     def get_id(self):
         """Returns the ID of the work shift."""
         return self._id
+
     def set_id(self, new_id):
         """Sets the ID of the work shift."""
         self._id = new_id
 
     @classmethod
-    def from_dict(self, d):
+    def from_dict(cls, d):
         """
-        The function is a class method that takes in a dictionary
-        and returns an instance of the class.
+        Creates an instance of ServiceShift from a dictionary.
         """
-        return self(**d)
+        return cls(**d)
 
     def to_dict(self):
         """
-        The function takes an object and returns a dictionary
-        representation of the object.
+        Converts the ServiceShift instance into a dictionary.
         """
         return dataclasses.asdict(self)
-    
