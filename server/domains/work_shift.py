3b01387cc61c0f17c4cf6862547acb4be1f9efde
strejoarciles <strejoarciles@slu.edu>
2025-02-09 19:07:41 -0600

    removed comments

--- server/domains/work_shift.py
+++ server/domains/work_shift.py
@@ -3,12 +3,16 @@ This module handles data converstion from dictionary to class obj or vice versa
 """
 import uuid
 import dataclasses
+from deprecated import deprecated
 
-
+@deprecated(
+    reason="WorkShift is deprecated and will be removed in a future release. "
+           "Use ServiceShift instead."
+)
 @dataclasses.dataclass
 class WorkShift:
     """
-    Data class for workshift releated data
+    **DEPRECATED:** Use `ServiceShift` instead.
     """
     worker: str
     first_name: str
@@ -18,14 +22,18 @@ class WorkShift:
     end_time: int
     _id: uuid.UUID = None
 
+    @deprecated(reason="get_id() is deprecated along with WorkShift.")
     def get_id(self):
         """Returns the ID of the work shift."""
         return self._id
+    
+    @deprecated(reason="set_id() is deprecated along with WorkShift.")
     def set_id(self, new_id):
         """Sets the ID of the work shift."""
         self._id = new_id
 
     @classmethod
+    @deprecated(reason="from_dict() is deprecated along with WorkShift. Use ServiceShift.from_dict instead.")
     def from_dict(self, d):
         """
         The function is a class method that takes in a dictionary
@@ -33,6 +41,7 @@ class WorkShift:
         """
         return self(**d)
 
+    @deprecated(reason="to_dict() is deprecated along with WorkShift. Use ServiceShift.to_dict instead.")
     def to_dict(self):
         """
         The function takes an object and returns a dictionary
