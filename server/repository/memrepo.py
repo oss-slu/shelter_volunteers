383e8649692b03c71386f411e55d0201ded5d95c
Kate Holdener <kate.holdener@slu.edu>
2025-02-10 11:22:17 -0600

    undoing changes to memrepo.py

--- server/repository/memrepo.py
+++ server/repository/memrepo.py
@@ -1,12 +1,12 @@
 """
 This module is for in-memory repository implementation.
 """
-from domains.service_shift import ServiceShift
+from domains.work_shift import WorkShift
 
 
 class MemRepo:
     """
-    An in-memory repository for storing service shifts.
+    An in-memory repository for storing work shifts.
     """
 
     def __init__(self, data):
@@ -17,23 +17,23 @@ class MemRepo:
 
     def list(self, user=None, shelter=None):
         """
-        Return a list of ServiceShift objects based on the data.
+        Return a list of WorkShift objects based on the data.
         """
-        shifts = [ServiceShift.from_dict(i) for i in self.data if \
+        shifts = [WorkShift.from_dict(i) for i in self.data if \
                   (user is None or i["worker"] == user) and \
                   (shelter is None or i["shelter"].shelter == shelter)]
         return shifts
 
-    def add(self, service_shift):
+    def add(self, work_shift):
         """
-        Add a ServiceShift object to the data.
+        Add a WorkShift object to the data.
         """
-        self.data.append(service_shift)
+        self.data.append(work_shift)
 
     def get_by_id(self, shift_id):
         for item in self.data:
             if item["_id"] == shift_id:
-                return ServiceShift.from_dict(item)
+                return WorkShift.from_dict(item)
         return None
 
     def delete(self, shift_id):
