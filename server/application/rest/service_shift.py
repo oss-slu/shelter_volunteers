5b8378a230762f02f6fb5bdd538851db1854797b
Amal Rizvi <amal.rizvi@slu.edu>
2025-02-17 12:18:40 -0600

    fixing quotation linting error

--- server/application/rest/service_shift.py
+++ server/application/rest/service_shift.py
@@ -9,14 +9,18 @@ repo = MongoRepo()
 
 @service_shift_bp.route('/service_shift', methods=['POST'])
 def create_service_shift():
-    """shift creation requests here""" 
+    """
+    shift creation requests here
+    """ 
     data = request.get_json()
     new_shift = ServiceShift.from_dict(data)
     existing_shifts = repo.get_shifts_for_volunteer(data.get("user_id"))
     
     response = shift_add_use_case(repo, new_shift, existing_shifts)
     return jsonify(response)
-"""need to check here, does this route to the correct id?"""
+"""
+need to check here, does this route to the correct id?
+"""
 @service_shift_bp.route('/service_shift/shelter_id/<int:shelter_id>', methods=['GET'])
 def retrieve_service_shifts(shelter_id):
     """based on ID, fetch the shifts"""
