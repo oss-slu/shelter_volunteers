c0f4f89b1c80dfdd5e47cb5ecdd1ca62a7e52529
Kate Holdener <kate.holdener@slu.edu>
2025-02-25 12:40:04 -0600

    adding missing files

---
+++ server/use_cases/list_service_commitments.py
@@ -0,0 +1,22 @@
+"""
+This module contains the use case for listing service commitments.
+"""
+
+def list_service_commitments(repo, user_email):
+    """
+    Retrieves all service commitments for a given user.
+
+    Args:
+        repo: A repository object that provides a 
+        fetch_service_commitments method.
+        user_email (str): The user's email.
+
+    Returns:
+        list: A list of service commitment dictionaries 
+        with IDs and associated service shift IDs.
+    """
+    commitments = repo.fetch_service_commitments(user_email)
+    return commitments
+    # return [{"service_commitment_id": c["_id"],
+    #          "service_shift_id": c["service_shift_id"]}
+    #         for c in commitments]
