e2525594ec99904c183c6797f8c35d7a5dcf6cd4
Kate Holdener <kate.holdener@slu.edu>
2025-02-25 12:47:58 -0600

    moved the list_service_commitments function into a separate file

--- server/use_cases/add_service_commitments.py
+++ server/use_cases/add_service_commitments.py
@@ -23,22 +23,3 @@ def add_service_commitments(repo, commitments):
         "service_commitment_id": str(c["_id"])
         , "success": True}
             for c in commitments_as_dict]
-
-
-def get_service_commitments(repo, user_email):
-    """
-    Retrieves all service commitments for a given user.
-
-    Args:
-        repo: A repository object that provides a 
-        fetch_service_commitments method.
-        user_email (str): The user's email.
-
-    Returns:
-        list: A list of service commitment dictionaries 
-        with IDs and associated service shift IDs.
-    """
-    commitments = repo.fetch_service_commitments(user_email)
-    return [{"service_commitment_id": c["service_commitment_id"],
-             "service_shift_id": c["service_shift_id"]}
-            for c in commitments]
