24ea73019c913239fa3893f744b2f499b5c1fb19
Kate Holdener <42073871+kate-holdener@users.noreply.github.com>
2025-02-27 11:41:13 -0600

    refactored the add_service_commitments and list_service_commitments use cases (#193)
    * refactored the add_service_commitments and list_service_commitments use cases to accept two repos as arguments: one for commitments and one for shifts. This is needed because we'll need to do a variety of checks on the commitments when we add them and we'll need to supply shift data when we retrieve commitments

--- server/use_cases/list_service_commitments.py
+++ server/use_cases/list_service_commitments.py
@@ -2,18 +2,22 @@
 This module contains the use case for listing service commitments.
 """
 
-def list_service_commitments(repo, user_email):
+def list_service_commitments(commitments_repo, shifts_repo, user_email=None):
     """
     Retrieves all service commitments for a given user.
 
     Args:
-        repo: A repository object that provides a 
-        fetch_service_commitments method.
+        commitments_repo: A repository object that contains 
+                          all service commitments
+        shifts_repo: A repository object that contains all service shifts
         user_email (str): The user's email.
 
     Returns:
         list: A list of service commitment dictionaries 
         with IDs and associated service shift IDs.
     """
-    commitments = repo.fetch_service_commitments(user_email)
-    return commitments
+
+    commitments = commitments_repo.fetch_service_commitments(user_email)
+    shift_ids = [commitment.service_shift_id for commitment in commitments]
+    shifts = shifts_repo.get_shifts(shift_ids)
+    return (commitments, shifts)
