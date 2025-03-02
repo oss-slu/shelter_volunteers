16c5667ae0eed1389ba58c7bf386e2df00b14bb6
Kate Holdener <kate.holdener@slu.edu>
2025-03-02 17:06:37 -0600

    moved files

--- server/application/rest/work_shift.py
+++ server/application/rest/work_shift.py
@@ -55,15 +55,6 @@ shifts = [
 
 ]
 
-HTTP_STATUS_CODES_MAPPING = {
-    ResponseTypes.NOT_FOUND: 404,
-    ResponseTypes.SYSTEM_ERROR: 500,
-    ResponseTypes.AUTHORIZATION_ERROR: 403,
-    ResponseTypes.PARAMETER_ERROR: 400,
-    ResponseTypes.SUCCESS: 200,
-    ResponseTypes.CONFLICT: 409
-}
-
 @blueprint.route("/getvolunteers/<int:shelter_id>", methods=["GET"])
 @cross_origin()
 def get_volunteers(shelter_id):
