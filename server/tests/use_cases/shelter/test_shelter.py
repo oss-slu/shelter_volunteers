75cdc966726cefa17c53d2f83220cbadfa00ffe2
Henry Meiners <hrmeiners01@gmail.com>
2025-03-04 16:02:19 -0600

    my attempt at writing a mock test case

--- server/tests/use_cases/shelter/test_shelter.py
+++ server/tests/use_cases/shelter/test_shelter.py
@@ -1,1 +1,21 @@
-# placeholder
+"""
+Module containing test cases for adding work shifts.
+"""
+
+import pytest
+from unittest import mock
+from domains.shelter.shelter import Shelter
+
+
+class MockShelterRepo:
+    def add(self, shelter):
+        shelter_id = shelter.pop("_id")
+        self.collection.insert_one(shelter)
+        
+        return  {
+                    'id': shelter_id,
+                    'success': 'true',
+                    'message': 'shelter added successfully'
+                }
+
+        
