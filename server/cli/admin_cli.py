8927790a73afe3d7e4296380c4e6402ea4bc5b18
Kate Holdener <kate.holdener@slu.edu>
2025-03-16 11:53:57 -0500

    fixed linter reported errors

--- server/cli/admin_cli.py
+++ server/cli/admin_cli.py
@@ -1,3 +1,6 @@
+"""
+This script is used to create system and shelter admins from the command line.
+"""
 import argparse
 from use_cases.authorization.add_system_admin import add_system_admin
 from use_cases.authorization.add_shelter_admin import add_shelter_admin
@@ -6,36 +9,53 @@ from responses import ResponseTypes
 
 def create_system_admin(email):
     permissions_repo = PermissionsMongoRepo()
-    try:
-        response = add_system_admin(permissions_repo, email)
-        if response.response_type == ResponseTypes.SUCCESS:
-            print(f"System admin permissions granted to {email}")
-        else:
-            print(response.value)
-    except Exception as e:
-        print(f"Failed to grant system admin permissions: {e}")
+    response = add_system_admin(permissions_repo, email)
+    if response.response_type == ResponseTypes.SUCCESS:
+        print(f"System admin permissions granted to {email}")
+    else:
+        print(response.value)
+
 
 def create_shelter_admin(email, shelter_id):
     permissions_repo = PermissionsMongoRepo()
-    try:
-        response = add_shelter_admin(permissions_repo, shelter_id, email)
-        if response.response_type == ResponseTypes.SUCCESS:
-            print(f"Shelter admin permissions granted to {email} for shelter {shelter_id}")
-        else:
-            print(response.value)
-    except Exception as e:
-        print(f"Failed to grant shelter admin permissions: {e}")
+    response = add_shelter_admin(permissions_repo, shelter_id, email)
+    if response.response_type == ResponseTypes.SUCCESS:
+        print(f"Shelter admin permissions granted to {email} for shelter {shelter_id}")
+    else:
+        print(response.value)
 
 def main():
-    parser = argparse.ArgumentParser(description='Create a system admin or shelter admin by email address.')
-    subparsers = parser.add_subparsers(dest='admin_type', required=True, help='Type of admin to create')
+    parser = argparse.ArgumentParser(
+        description='Create a system admin or shelter admin by email address.'
+        )
+    subparsers = parser.add_subparsers(
+        dest='admin_type',
+        required=True, help='Type of admin to create'
+        )
 
-    system_admin_parser = subparsers.add_parser('system', help='Create a system admin')
-    system_admin_parser.add_argument('email', type=str, help='The email address of the user to be granted system admin permissions')
+    system_admin_parser = subparsers.add_parser(
+        'system',
+        help='Create a system admin'
+        )
+    system_admin_parser.add_argument(
+        'email',
+        type=str,
+        help='The email address of the user to be granted system admin permissions'
+        )
 
-    shelter_admin_parser = subparsers.add_parser('shelter', help='Create a shelter admin')
-    shelter_admin_parser.add_argument('email', type=str, help='The email address of the user to be granted shelter admin permissions')
-    shelter_admin_parser.add_argument('shelter_id', type=str, help='The ID of the shelter')
+    shelter_admin_parser = subparsers.add_parser(
+        'shelter',
+        help='Create a shelter admin'
+        )
+    shelter_admin_parser.add_argument(
+        'email',
+        type=str,
+        help='The email address of the user to be granted shelter admin permissions'
+        )
+    shelter_admin_parser.add_argument(
+        'shelter_id',
+        type=str, help='The ID of the shelter'
+        )
 
     args = parser.parse_args()
 
