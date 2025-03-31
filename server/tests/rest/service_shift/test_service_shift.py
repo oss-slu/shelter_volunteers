fdea6579f2138a4da99de5073a7dedbbfbcc59c9
Amal Rizvi <amal.rizvi@slu.edu>
2025-03-30 22:43:07 -0500

    service_test file

--- server/tests/rest/service_shift/test_service_shift.py
+++ server/tests/rest/service_shift/test_service_shift.py
@@ -81,7 +81,8 @@ class TestServiceShiftAPI(unittest.TestCase):
                 "shift_name": "Default Shift"
             }
         ]
-        mock_list_use_case.return_value = [ServiceShift.from_dict(shift) for shift in expected_shifts]
+        mock_list_use_case.return_value = [ServiceShift.from_dict(shift) 
+                                           for shift in expected_shifts]
 
         # Act: Send GET request.
         response = self.client.get("/service_shift")
@@ -113,7 +114,8 @@ class TestServiceShiftAPI(unittest.TestCase):
           "can_sign_up": True,
           "shift_name": "Default Shift"
         }
-        mock_list_use_case.return_value = [ServiceShift.from_dict(expected_shift)]
+        mock_list_use_case.return_value = [ServiceShift.from_dict(
+            expected_shift)]
 
         # Act: Send GET request with shelter_id filter
         response = self.client.get(
