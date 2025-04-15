7ab8b358c47906d57cb3ed43452931cfeae407cd
Amal Rizvi <amal.rizvi@slu.edu>
2025-04-15 11:54:18 -0500

    linting fixes

--- server/application/rest/shelter.py
+++ server/application/rest/shelter.py
@@ -44,7 +44,8 @@ def shelter():
             )
         if "name" not in shelter_data_dict or not shelter_data_dict['name']:
             return Response(
-                json.dumps({"success": False, "message": "Missing required field: name"}),
+                json.dumps({"success": False, "message": 
+                            "Missing required field: name"}),
                 mimetype="application/json",
                 status=HTTP_STATUS_CODES_MAPPING[ResponseTypes.PARAMETER_ERROR]
             )
@@ -52,7 +53,9 @@ def shelter():
             # shelter_add_use_case expects a Shelter object
             shelter_obj = Shelter.from_dict(shelter_data_dict)
             add_response = shelter_add_use_case(repo, shelter_obj)
-            status_code = HTTP_STATUS_CODES_MAPPING[ResponseTypes.SUCCESS] if add_response["success"] else HTTP_STATUS_CODES_MAPPING[ResponseTypes.PARAMETER_ERROR]
+            status_code = HTTP_STATUS_CODES_MAPPING[
+                ResponseTypes.SUCCESS] if add_response[
+                    "success"] else HTTP_STATUS_CODES_MAPPING[ResponseTypes.PARAMETER_ERROR]
         except ValueError as e:
             # Handle validation errors from Shelter.from_dict
             add_response = {"success": False, "message": str(e)}
