7bcf2966c03fa248d892ab5c4658677231e8e9f0
Kate Holdener <kate.holdener@slu.edu>
2025-04-30 15:34:09 -0500

    updated functionality allowing volunteers to create service commitments from service shifts using the client side application

---
+++ client_app/src/api/serviceCommitment.js
@@ -0,0 +1,36 @@
+import { fetchClient } from "./fetchClient";
+
+export const serviceCommitmentAPI = {
+    getCommitments: async () => {
+        const response = await fetchClient(`/service_commitmentß`);
+        return response;
+    },
+    getFutureCommitments: async () => {
+        // sends a GET request to /service_commitment endpoint
+        // passing the current time (in milliseconds since epoch) as a
+        // value for the filter_start_after query parameter
+        const timeNow = Date.now();
+        const response = await fetchClient(
+            `/service_commitment?filter_start_after=${timeNow}`
+        );
+        return response;
+    },
+    getPastCommitments: async() => {
+        // sends a GET request to /service_commitment endpoint
+        // passing the current time (in milliseconds since epoch) as a
+        // value for the filter_start_before query parameter
+        const timeNow = Date.now();
+        const response = await fetchClient(
+            `/service_commitment?filter_start_before=${timeNow}`
+        );
+        return response;
+    },
+    addCommitments: async (data) => {
+        console.log("Adding commitment", data);
+        const response = await fetchClient("/service_commitment", {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+        return response;
+    },
+};
