d4652f9d75a8efd9cbdeafb85a8d272f991a0f38
Emra Meduseljac <emmy@Mac.search.charter.net>
2025-05-04 19:39:59 -0500

    extend ScheduleMongoRepo from ServiceShiftsMongoRepo and add unit test for /schedule endpoint

--- server/repository/mongo/schedule_repo.py
+++ server/repository/mongo/schedule_repo.py
@@ -1,11 +1,6 @@
-from domains.service_shift import ServiceShift
-from config.mongodb_config import get_db
+from repository.mongo.service_shifts import ServiceShiftsMongoRepo
 
-class ScheduleMongoRepo:
+class ScheduleMongoRepo(ServiceShiftsMongoRepo):
     def __init__(self):
-        self.db = get_db()
-        self.collection = self.db.schedules
-
-    def list_by_shelter_id(self, shelter_id):
-        shifts = self.collection.find({"shelter_id": shelter_id})
-        return [ServiceShift.from_dict(s) for s in shifts]
+        super().__init__()
+        self.collection = self.db.schedules  # override to use 'schedules' collection
