580dffad0332e1412f93eafcc2c8fac68f8b79ef
Emra Meduseljac <emmy@Mac.search.charter.net>
2025-05-04 21:01:04 -0500

    importing patch

--- server/tests/rest/test_schedule_endpoint.py
+++ server/tests/rest/test_schedule_endpoint.py
@@ -1,6 +1,7 @@
 """Unit tests for the /schedule GET endpoint using mocked repository."""
 
 import unittest
+from unittest.mock import patch
 from application.app import create_app
 from domains.service_shift import ServiceShift
 
