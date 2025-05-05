b36ff7f828a6ddc7c5dac1264c431edadef088f6
Kate Holdener <kate.holdener@slu.edu>
2025-05-05 09:19:47 -0500

    work in progress

--- server/use_cases/list_service_shifts_use_case.py
+++ server/use_cases/list_service_shifts_use_case.py
@@ -1,8 +1,13 @@
 """
 This module contains the use case for listing shifts.
 """
-
-def service_shifts_list_use_case(repo, shelter=None, filter_start_after=None):
+from use_cases.list_service_commitments import list_service_commitments
+from request.time_filter import build_time_filter
+def service_shifts_list_use_case(
+        shifts_repo,
+        commitments_repo,
+        shelter=None,
+        filter_start_after=None):
     """
     Retrieves service shifts, filtered by shelter and optional start time.
 
@@ -14,4 +19,14 @@ def service_shifts_list_use_case(repo, shelter=None, filter_start_after=None):
     Returns:
         list: A list of service shift records.
     """
-    return repo.list(shelter, filter_start_after)
+    service_shifts = shifts_repo.list(shelter, filter_start_after)
+    time_filter = build_time_filter()
+
+    for shift in service_shifts:
+        commitments = list_service_commitments(
+            commitments_repo,
+            shifts_repo,
+            time_filter,
+            None,
+            shift.get_id()
+        )
