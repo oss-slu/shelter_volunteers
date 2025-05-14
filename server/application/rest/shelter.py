a0fb3d44a288ef5cd53336bda090b2bab3e363fc
Amal Rizvi <amal.rizvi@slu.edu>
2025-05-14 00:48:16 -0500

    raise value error for missing fields and return with 400

--- server/application/rest/shelter.py
+++ server/application/rest/shelter.py
@@ -35,15 +35,27 @@ def shelter():
             status=HTTP_STATUS_CODES_MAPPING[ResponseTypes.SUCCESS]
         )
     elif request.method == "POST":
-        shelter_data_dict = request.get_json()
-        # shelter_add_use_case expects a Shelter object
-        shelter_obj = Shelter.from_dict(shelter_data_dict)
-        add_response = shelter_add_use_case(repo, shelter_obj)
-        status_code = HTTP_STATUS_CODES_MAPPING[ResponseTypes.PARAMETER_ERROR]
-        if add_response["success"]:
-            status_code = HTTP_STATUS_CODES_MAPPING[ResponseTypes.SUCCESS]
-        return Response(
-            json.dumps(add_response, default=str),
-            mimetype="application/json",
-            status=status_code
-        )
+        try: 
+            shelter_data_dict = request.get_json()
+            # shelter_add_use_case expects a Shelter object
+            shelter_obj = Shelter.from_dict(shelter_data_dict)
+            add_response = shelter_add_use_case(repo, shelter_obj)
+            status_code = HTTP_STATUS_CODES_MAPPING[ResponseTypes.PARAMETER_ERROR]
+            if add_response["success"]:
+                status_code = HTTP_STATUS_CODES_MAPPING[ResponseTypes.SUCCESS]
+            return Response(
+                json.dumps(add_response, default=str),
+                mimetype="application/json",
+                status=status_code
+            )
+        except ValueError as e:
+                #for validation errors
+                error_response = {
+                    "success": False,
+                    "message": str(e)
+                }
+                return Response(
+                    json.dumps(error_response),
+                    mimetype="application/json",
+                    status=HTTP_STATUS_CODES_MAPPING[ResponseTypes.PARAMETER_ERROR]
+                )
