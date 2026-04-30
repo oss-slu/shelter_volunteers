d5cf01979d8df8dfa04e4ce40c68d3aa6ffa9ab4
Supraja050202 <Suprajachitmilla100@gmail.com>
2026-04-30 13:52:28 -0500

    clientid

--- client_app/src/config.js
+++ client_app/src/config.js
@@ -1,6 +1,6 @@
 export const SERVER = process.env.REACT_APP_SERVER_URL !== undefined ?
     process.env.REACT_APP_SERVER_URL : 'http://localhost:5001'
 export const ENVIROMENT = "development";
-export const REACT_APP_GOOGLE_CLIENT_ID = "214227450442-gp12srosdps9365qbfum19dvnutu9gi5.apps.googleusercontent.com";
+export const REACT_APP_GOOGLE_CLIENT_ID = "214227450442-gp12srosdps9365qbfum19dvnutu9gi5.apps.googleusercontent.com"
 
 
